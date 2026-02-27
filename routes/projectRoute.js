012c3990cc05f2efab781d8572a059814ad7de8e
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-27 02:01:06 +0200

    feat: add new route for stats

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -6,20 +6,17 @@ import {
     getProjectById,
     updateProjectById,
     deleteProjectById,
-} from '../controllers/projectController.js';
-import {
-    auth,
-    allowedTo,
-} from '../controllers/authController.js';
-
+    getProjectStats,
+} from "../controllers/projectController.js";
+import { auth, allowedTo } from "../controllers/authController.js";
 
 const router = express.Router();
 
+router.get("/stats", auth, allowedTo("manager", "admin"), getProjectStats);
+router.get("/", auth, allowedTo("manager", "admin"), getAllProjects);
 router.post("/", auth, allowedTo("manager"), createProject);
-router.get("/", auth, allowedTo("manager", "admin"), getAllProjects)
 router.get("/:id", auth, allowedTo("manager", "admin"), getProjectById);
 router.patch("/:id", auth, allowedTo("manager"), updateProjectById);
 router.delete("/:id", auth, allowedTo("manager"), deleteProjectById);
-
 
 export default router;

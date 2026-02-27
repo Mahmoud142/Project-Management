6271785162f678404616e2a301711fc65eca23cc
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-27 04:51:08 +0200

    feat: apply caching middleware to get projects stats

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -9,13 +9,13 @@ import {
     getProjectStats,
 } from "../controllers/projectController.js";
 import { auth, allowedTo } from "../controllers/authController.js";
-
+import { cacheData } from "../middlewares/cacheMiddleware.js";
 const router = express.Router();
 
-router.get("/stats", auth, allowedTo("manager", "admin"), getProjectStats);
+router.get("/stats", auth, allowedTo("manager", "admin"), cacheData(100), getProjectStats);
 router.get("/", auth, allowedTo("manager", "admin"), getAllProjects);
 router.post("/", auth, allowedTo("manager"), createProject);
-router.get("/:id", auth, allowedTo("manager", "admin"), getProjectById);
+router.get("/:id", auth, allowedTo("manager", "admin"),  getProjectById);
 router.patch("/:id", auth, allowedTo("manager"), updateProjectById);
 router.delete("/:id", auth, allowedTo("manager"), deleteProjectById);
 

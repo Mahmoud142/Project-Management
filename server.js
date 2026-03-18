c92b56d2214fac01836e18caedac97d96440f3f6
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-03-18 17:47:48 +0200

    test
    Co-authored-by: Anything <mabdellah307@gmail.com>

--- server.js
+++ server.js
@@ -173,6 +173,7 @@ app.use(globalError);
 
 dbConnection();
 connectRedis();
+
 server.listen(PORT, () => {
     console.log(
         `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`,
